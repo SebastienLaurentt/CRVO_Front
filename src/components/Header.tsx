8f6fab1cf272062f530fb2068628d27e15e9835b
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-11 20:56:09 +0200

    add home link button in header only if admin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,29 @@ import { useAuth } from "@/lib/auth";
 import CRVOLogo from "/public/images/CRVOLogo.png";
 
 const Header = () => {
-  const { logout } = useAuth();
+  const { role, logout } = useAuth(); // Assurez-vous que useAuth retourne user avec le rôle
 
   const handleLogout = async () => {
     logout();
   };
 
   return (
-    <header className="max-w-7xl px-4 mx-auto w-full flex justify-between items-center  py-4 ">
-      <img src={CRVOLogo} alt="Logo CRVO" className="w-24 " />
-      <Button asChild onClick={handleLogout} variant="destructive">
-        <Link to="/" aria-label="Se déconnecter de l'espace administrateur">
-          Déconnexion
-        </Link>
-      </Button>
+    <header className="max-w-7xl px-4 mx-auto w-full flex justify-between items-center py-4">
+      <img src={CRVOLogo} alt="Logo CRVO" className="w-24" />
+      <div className="flex items-center gap-4">
+        {role === "admin" && (
+          <Button asChild>
+            <Link to="/" aria-label="Revenir à l'accueil">
+              Tableau de Bord
+            </Link>
+          </Button>
+        )}
+        <Button asChild onClick={handleLogout} variant="destructive">
+          <Link to="/" aria-label="Se déconnecter de l'espace administrateur">
+            Déconnexion
+          </Link>
+        </Button>
+      </div>
     </header>
   );
 };
