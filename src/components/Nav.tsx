1504e8018d0f70e3f7820dc0193cbf3ab2286a94
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-20 18:13:33 +0200

    update navDisplay

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,38 +12,39 @@ const Nav = () => {
   };
 
   return (
-    <nav className="px-4 h-[600px] w-[300px] flex flex-col justify-between">
+    <nav className="px-4 h-[600px] w-[300px] flex flex-col justify-between mt-20">
       <div>
         <img src={CRVOLogo} alt="Logo CRVO" className="w-full mb-8" />
         <div className="flex flex-col">
           <div className="mb-4">
             <span className="text-slate-700 font-semibold mb-1">Véhicules</span>
+            <div className="space-y-1">
+              <NavLink
+                to="/"
+                aria-label="Revenir à l'accueil"
+                className={({ isActive }) =>
+                  `flex flex-row gap-x-2 items-center font-bold border p-2 rounded-md hover:bg-slate-950 hover:text-slate-50 ${
+                    isActive ? "bg-slate-900 text-slate-50" : ""
+                  }`
+                }
+              >
+                <LoaderCircle size={24} />
+                <span>Véhicules En Cours</span>
+              </NavLink>
 
-            <NavLink
-              to="/"
-              aria-label="Revenir à l'accueil"
-              className={({ isActive }) =>
-                `flex flex-row gap-x-2 items-center font-bold p-2 rounded-md hover:bg-primary ${
-                  isActive ? "bg-primary" : ""
-                }`
-              }
-            >
-              <LoaderCircle size={24} />
-              <span>En Cours</span>
-            </NavLink>
-
-            <NavLink
-              to="/completed"
-              aria-label="Terminé"
-              className={({ isActive }) =>
-                `flex flex-row gap-x-2 items-center font-bold p-2 rounded-md hover:bg-primary ${
-                  isActive ? "bg-primary" : ""
-                }`
-              }
-            >
-              <ArrowRightToLine size={24} />
-              <span>Terminé</span>
-            </NavLink>
+              <NavLink
+                to="/completed"
+                aria-label="Terminé"
+                className={({ isActive }) =>
+                  `flex flex-row gap-x-2 items-center font-bold p-2 border rounded-md hover:bg-slate-950 hover:text-slate-50 ${
+                    isActive ? "bg-slate-900 text-slate-50" : ""
+                  }`
+                }
+              >
+                <ArrowRightToLine size={24} />
+                <span>Véhicules Terminés</span>
+              </NavLink>
+            </div>
           </div>
           {role === "admin" && (
             <>
@@ -51,8 +52,8 @@ const Nav = () => {
               <NavLink
                 to="/clients"
                 className={({ isActive }) =>
-                  `flex flex-row gap-x-2 items-center font-bold p-2 rounded-md hover:bg-primary ${
-                    isActive ? "bg-primary" : ""
+                  `flex flex-row gap-x-2 items-center font-bold border p-2 rounded-md hover:bg-slate-950 hover:text-slate-50 ${
+                    isActive ? "bg-slate-900 text-slate-50" : ""
                   }`
                 }
               >
@@ -61,26 +62,25 @@ const Nav = () => {
               </NavLink>
             </>
           )}
+          {role === "member" && (
+            <>
+              <span className="text-slate-700 font-semibold mb-1">Clients</span>
+              <Button className="space-x-[5px]" asChild>
+                <Link to="https://facturation.crvo.fr/" target="_blank">
+                  <BookText size={20} />
+                  <span>Factures</span>
+                </Link>
+              </Button>
+            </>
+          )}
         </div>
       </div>
-      <div className="flex flex-col gap-y-2">
-        {role === "member" && (
-          <Button className="space-x-[5px]" asChild>
-            <Link to="https://facturation.crvo.fr/" target="_blank">
-              <BookText size={20} />
-              <span>Factures</span>
-            </Link>
-          </Button>
-        )}
-        <Button asChild onClick={handleLogout} variant="destructive">
-          <NavLink
-            to="/"
-            aria-label="Se déconnecter de l'espace administrateur"
-          >
-            Déconnexion
-          </NavLink>
-        </Button>
-      </div>
+
+      <Button asChild onClick={handleLogout} variant="destructive">
+        <NavLink to="/" aria-label="Se déconnecter de l'espace administrateur">
+          Déconnexion
+        </NavLink>
+      </Button>
     </nav>
   );
 };
