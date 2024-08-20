05692a829e1ae5cf54a65be149c2c5ff1a00fa39
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-21 00:36:38 +0200

    remove header and footer component

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="flex font-bold flex-col gap-y-2 items-center w-full sm:px-8 px-4 py-4">
-      CRVO - Lyon @ 2024
-    </footer>
-  );
-};
-
-export default Footer;
