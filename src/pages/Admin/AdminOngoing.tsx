f42ac781b7513b79555f4a7ce5d6a469ec844967
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-22 19:55:26 +0200

    add select to filter prepa col

--- src/pages/Admin/AdminOngoing.tsx
+++ src/pages/Admin/AdminOngoing.tsx
@@ -2,6 +2,13 @@ import AddExcelData from "@/components/AddExcelData";
 import Loader from "@/components/Loader";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useQuery } from "@tanstack/react-query";
 import {
   AudioLines,
@@ -19,6 +26,7 @@ interface Vehicle {
   _id: string;
   immatriculation: string;
   modele: string;
+  vin: string;
   dateCreation: string;
   user: {
     username: string;
@@ -50,6 +58,7 @@ const fetchVehicles = async (): Promise<Vehicle[]> => {
 const AdminOngoing: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [isFileInputVisible, setIsFileInputVisible] = useState(false);
+  const [selectedColumn, setSelectedColumn] = useState<string>("Tout");
 
   const {
     data: vehicles,
@@ -66,7 +75,8 @@ const AdminOngoing: React.FC = () => {
     return (
       vehicle.immatriculation.toLowerCase().includes(searchLower) ||
       vehicle.modele.toLowerCase().includes(searchLower) ||
-      vehicle.user.username.toLowerCase().includes(searchLower)
+      vehicle.user.username.toLowerCase().includes(searchLower) ||
+      vehicle.vin.toLowerCase().includes(searchLower)
     );
   });
 
@@ -87,7 +97,28 @@ const AdminOngoing: React.FC = () => {
             onChange={(e) => setSearchQuery(e.target.value)}
           />
         </div>
-        <div className="flex flex-row gap-x-2">
+        <Select
+          value={selectedColumn}
+          onValueChange={(value) => setSelectedColumn(value)}
+        >
+          <SelectTrigger className="w-[200px]">
+            <SelectValue>
+              {selectedColumn === "" || selectedColumn === "Tout"
+                ? "Étape de préparation"
+                : selectedColumn}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Tout">Tout</SelectItem>
+            <SelectItem value="DSP">DSP</SelectItem>
+            <SelectItem value="Mécanique">Mécanique</SelectItem>
+            <SelectItem value="Jantes">Jantes</SelectItem>
+            <SelectItem value="CT">CT</SelectItem>
+            <SelectItem value="Carrosserie">Carrosserie</SelectItem>
+            <SelectItem value="Esthétique">Esthétique</SelectItem>
+          </SelectContent>
+        </Select>
+        <div className="flex flex-row gap-x-2 items-center">
           <Button
             className="space-x-[5px]"
             onClick={() => setIsFileInputVisible(true)}
@@ -100,41 +131,56 @@ const AdminOngoing: React.FC = () => {
       <div className="relative">
         <div className="h-[550px] overflow-y-auto w-full">
           <table className="w-full">
-            <thead>
+            <thead >
               <tr className="text-left bg-background border-b sticky top-0 z-10">
                 <th className="py-3 px-2 2xl:px-6 w-[300px]">Client</th>
                 <th className="py-3 px-2 2xl:px-6 w-[200px]">
                   Immatriculation
                 </th>
                 <th className="py-3 px-2 2xl:px-6 w-[250px]">Modèle</th>
-                <th className="py-3 px-2 2xl:px-6 w-[200px] text-center">
+                <th className="py-3 px-2 2xl:px-6 w-[200px]">VIN</th>{" "}
+                <th className="py-3 px-2 2xl:px-4 text-center">
                   Jours depuis Création
                 </th>
-                <th className="py-3 px-4 w-[60px] text-center">
-                  <AudioLines className="inline-block mb-0.5" /> DSP
-                </th>
-                <th className="py-3 px-4 w-[100px] text-center">
-                  <Wrench className="inline-block mb-0.5" /> Mécanique
-                </th>
-                <th className="py-3 px-4 w-[60px] text-center">
-                  <LifeBuoy className="inline-block mb-0.5" /> Jantes
-                </th>
-                <th className="py-3 px-4 w-[60px] text-center">
-                  <ShieldCheck className="inline-block mb-0.5" /> CT
-                </th>
-                <th className="py-3 px-4 w-[100px] text-center">
-                  <Car className="inline-block mb-0.5" /> Carrosserie
-                </th>
-                <th className="py-3 px-4 w-[80px] text-center">
-                  <SprayCan className="inline-block mb-0.5" /> Esthétique
-                </th>
+                {selectedColumn === "Tout" || selectedColumn === "DSP" ? (
+                  <th className="py-3 px-4 w-[60px] text-center">
+                    <AudioLines className="inline-block mb-0.5" /> DSP
+                  </th>
+                ) : null}
+                {selectedColumn === "Tout" || selectedColumn === "Mécanique" ? (
+                  <th className="py-3 px-4 w-[100px] text-center">
+                    <Wrench className="inline-block mb-0.5" /> Mécanique
+                  </th>
+                ) : null}
+                {selectedColumn === "Tout" || selectedColumn === "Jantes" ? (
+                  <th className="py-3 px-4 w-[60px] text-center">
+                    <LifeBuoy className="inline-block mb-0.5" /> Jantes
+                  </th>
+                ) : null}
+                {selectedColumn === "Tout" || selectedColumn === "CT" ? (
+                  <th className="py-3 px-4 w-[60px] text-center">
+                    <ShieldCheck className="inline-block mb-0.5" /> CT
+                  </th>
+                ) : null}
+                {selectedColumn === "Tout" ||
+                selectedColumn === "Carrosserie" ? (
+                  <th className="py-3 px-4 w-[100px] text-center">
+                    <Car className="inline-block mb-0.5" /> Carrosserie
+                  </th>
+                ) : null}
+                {selectedColumn === "Tout" ||
+                selectedColumn === "Esthétique" ? (
+                  <th className="py-3 px-4 w-[80px] text-center">
+                    <SprayCan className="inline-block mb-0.5" /> Esthétique
+                  </th>
+                ) : null}
               </tr>
             </thead>
 
             <tbody>
               {isLoadingVehicles ? (
                 <tr>
-                  <td colSpan={10} className="text-center py-20">
+                  <td colSpan={11} className="text-center py-20">
                     <div className="flex items-center justify-center">
                       <Loader />
                     </div>
@@ -142,7 +188,7 @@ const AdminOngoing: React.FC = () => {
                 </tr>
               ) : isErrorVehicles ? (
                 <tr>
-                  <td colSpan={10} className="text-center py-8">
+                  <td colSpan={11} className="text-center py-8">
                     Error:{" "}
                     {errorVehicles instanceof Error
                       ? errorVehicles.message
@@ -159,56 +205,74 @@ const AdminOngoing: React.FC = () => {
                       {vehicle.immatriculation}
                     </td>
                     <td className="py-4 px-2 2xl:px-6">{vehicle.modele}</td>
+                    <td className="py-4 px-2 2xl:px-6">{vehicle.vin}</td>{" "}
+                    {/* Affichage du VIN */}
                     <td className="py-4 px-2 2xl:px-4 text-center">
                       {daysSince(vehicle.dateCreation)}
                     </td>
-                    <td className="py-4 px-4 text-center">
-                      {vehicle.dsp ? (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      ) : (
-                        <BadgeCheck className="inline-block text-[#16a34a]" />
-                      )}
-                    </td>
-                    <td className="py-4 px-4 text-center">
-                      {vehicle.mecanique ? (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      ) : (
-                        <BadgeCheck className="inline-block text-[#16a34a]" />
-                      )}
-                    </td>
-                    <td className="py-4 px-4 text-center">
-                      {vehicle.jantes ? (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      ) : (
-                        <BadgeCheck className="inline-block text-[#16a34a]" />
-                      )}
-                    </td>
-                    <td className="py-4 px-4 text-center">
-                      {vehicle.ct ? (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      ) : (
-                        <BadgeCheck className="inline-block text-[#16a34a]" />
-                      )}
-                    </td>
-                    <td className="py-4 px-4 text-center">
-                      {vehicle.carrosserie ? (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      ) : (
-                        <BadgeCheck className="inline-block text-[#16a34a]" />
-                      )}
-                    </td>
-                    <td className="py-4 px-4 text-center">
-                      {vehicle.esthetique ? (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      ) : (
-                        <BadgeCheck className="inline-block text-[#fbbf24]" />
-                      )}
-                    </td>
+                    {selectedColumn === "Tout" || selectedColumn === "DSP" ? (
+                      <td className="py-4 px-4 text-center">
+                        {vehicle.dsp ? (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        ) : (
+                          <BadgeCheck className="inline-block text-[#16a34a]" />
+                        )}
+                      </td>
+                    ) : null}
+                    {selectedColumn === "Tout" ||
+                    selectedColumn === "Mécanique" ? (
+                      <td className="py-4 px-4 text-center">
+                        {vehicle.mecanique ? (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        ) : (
+                          <BadgeCheck className="inline-block text-[#16a34a]" />
+                        )}
+                      </td>
+                    ) : null}
+                    {selectedColumn === "Tout" ||
+                    selectedColumn === "Jantes" ? (
+                      <td className="py-4 px-4 text-center">
+                        {vehicle.jantes ? (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        ) : (
+                          <BadgeCheck className="inline-block text-[#16a34a]" />
+                        )}
+                      </td>
+                    ) : null}
+                    {selectedColumn === "Tout" || selectedColumn === "CT" ? (
+                      <td className="py-4 px-4 text-center">
+                        {vehicle.ct ? (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        ) : (
+                          <BadgeCheck className="inline-block text-[#16a34a]" />
+                        )}
+                      </td>
+                    ) : null}
+                    {selectedColumn === "Tout" ||
+                    selectedColumn === "Carrosserie" ? (
+                      <td className="py-4 px-4 text-center">
+                        {vehicle.carrosserie ? (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        ) : (
+                          <BadgeCheck className="inline-block text-[#16a34a]" />
+                        )}
+                      </td>
+                    ) : null}
+                    {selectedColumn === "Tout" ||
+                    selectedColumn === "Esthétique" ? (
+                      <td className="py-4 px-4 text-center">
+                        {vehicle.esthetique ? (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        ) : (
+                          <BadgeCheck className="inline-block text-[#fbbf24]" />
+                        )}
+                      </td>
+                    ) : null}
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan={10} className="text-center pt-8 font-medium">
+                  <td colSpan={11} className="text-center pt-8 font-medium">
                     Aucune donnée disponible actuellement.
                   </td>
                 </tr>
