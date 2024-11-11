19406d0849de6ac6537ea9384fc9f5ebbe0fb66d
SebastienLaurentt <slaurent.26@gmail.com>
2024-11-11 21:19:18 +0100

    remove passwordData

--- src/pages/Admin/AdminData.tsx
+++ src/pages/Admin/AdminData.tsx
@@ -1,10 +1,6 @@
 import DashboardHeader from "@/components/DashboardHeader";
-import Loader from "@/components/Loader";
-import { PasswordChangePieChart } from "@/components/PasswordChangePieChart";
-import { StatusBarChart } from "@/components/StatusBarChart";
 import { ProductionStatusBarChart } from "@/components/ProductionStatusBarChart";
-import { useQuery } from "@tanstack/react-query";
-import Cookies from "js-cookie";
+import { StatusBarChart } from "@/components/StatusBarChart";
 import React, { useMemo } from "react";
 
 interface Vehicle {
@@ -24,13 +20,6 @@ interface Vehicle {
   esthetique: boolean;
 }
 
-interface User {
-  _id: string;
-  username: string;
-  role: string;
-  passwordChanged: boolean;
-}
-
 interface AdminDataProps {
   vehicles: Vehicle[] | undefined;
   isLoadingVehicles: boolean;
@@ -39,34 +28,7 @@ interface AdminDataProps {
   syncDate: Date | null | undefined;
 }
 
-const fetchMembers = async (): Promise<User[]> => {
-  const token = Cookies.get("token");
-  const response = await fetch("https://crvo-back.onrender.com/api/users", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  if (!response.ok) {
-    throw new Error("Erreur lors de la récupération des utilisateurs.");
-  }
-  const users = await response.json();
-  return users.filter((user: User) => user.role === "member");
-};
-
-const AdminData: React.FC<AdminDataProps> = ({
-  vehicles,
-  syncDate
-}) => {
-  const {
-    data: members,
-    isLoading: isLoadingMembers,
-    isError: isErrorMembers,
-    error: errorMembers,
-  } = useQuery({
-    queryKey: ["users"],
-    queryFn: fetchMembers,
-  });
-
+const AdminData: React.FC<AdminDataProps> = ({ vehicles, syncDate }) => {
   const vehiclesByStatus = useMemo(() => {
     if (!vehicles) return {};
     return vehicles.reduce((acc, vehicle) => {
@@ -76,15 +38,16 @@ const AdminData: React.FC<AdminDataProps> = ({
   }, [vehicles]);
 
   const getStatusCounts = useMemo(() => {
-    if (!vehicles) return {
-      dsp: 0,
-      mecanique: 0,
-      jantes: 0,
-      ct: 0,
-      carrosserie: 0,
-      esthetique: 0,
-    };
-    
+    if (!vehicles)
+      return {
+        dsp: 0,
+        mecanique: 0,
+        jantes: 0,
+        ct: 0,
+        carrosserie: 0,
+        esthetique: 0,
+      };
+
     return vehicles.reduce(
       (acc, vehicle) => {
         if (vehicle.statusCategory === "Production") {
@@ -101,57 +64,32 @@ const AdminData: React.FC<AdminDataProps> = ({
     );
   }, [vehicles]);
 
-  const productionVehiclesCount = useMemo(() => 
-    vehicles?.filter(v => v.statusCategory === "Production").length || 0
-  , [vehicles]);
-
-  if (isErrorMembers) {
-    return (
-      <div>
-        Erreur lors du chargement des données:{" "}
-        {errorMembers?.message}
-      </div>
-    );
-  }
-
-  const totalMembers = members?.length || 0;
-
-  const usersWithPasswordChanged =
-    members?.filter((member) => member.passwordChanged).length || 0;
-  const usersWithoutPasswordChanged = totalMembers - usersWithPasswordChanged;
+  const productionVehiclesCount = useMemo(
+    () =>
+      vehicles?.filter((v) => v.statusCategory === "Production").length || 0,
+    [vehicles]
+  );
 
   return (
     <div className="h-[650px] rounded-l-lg border bg-primary pb-8 2xl:h-[800px]">
       <DashboardHeader title="Graphiques" />
-      {isLoadingMembers ? (
-        <div className="flex items-center justify-center py-40">
-          <Loader />
-        </div>
-      ) : (
-        <>
-          <div className="px-8 py-4">
-            <p>
-              Dernière synchronisation:{" "}
-              <span className="font-medium">
-                {syncDate
-                  ? `${syncDate.toLocaleDateString()} - ${syncDate.toLocaleTimeString()}`
-                  : "Chargement..."}
-              </span>
-            </p>
-          </div>
-          <div className="mt-4 grid grid-cols-3 gap-8 px-8">
-            <StatusBarChart vehiclesByStatus={vehiclesByStatus} />
-            <ProductionStatusBarChart 
-              productionCounts={getStatusCounts} 
-              productionVehiclesCount={productionVehiclesCount}
-            />
-            <PasswordChangePieChart
-              usersWithPasswordChanged={usersWithPasswordChanged}
-              usersWithoutPasswordChanged={usersWithoutPasswordChanged}
-            />
-          </div>
-        </>
-      )}
+      <div className="px-8 py-4">
+        <p>
+          Dernière synchronisation:{" "}
+          <span className="font-medium">
+            {syncDate
+              ? `${syncDate.toLocaleDateString()} - ${syncDate.toLocaleTimeString()}`
+              : "Chargement..."}
+          </span>
+        </p>
+      </div>
+      <div className="mt-4 grid grid-cols-3 gap-8 px-8">
+        <StatusBarChart vehiclesByStatus={vehiclesByStatus} />
+        <ProductionStatusBarChart
+          productionCounts={getStatusCounts}
+          productionVehiclesCount={productionVehiclesCount}
+        />
+      </div>
     </div>
   );
 };
